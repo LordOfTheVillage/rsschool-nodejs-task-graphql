b9574b93dd8a01ab44b7be737199d4c17be65d02
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-20 23:19:59 +0300

    feat: add rules

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,41 +1,46 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import {buildSchema, graphql} from 'graphql';
+import {FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
+import {createGqlResponseSchema, gqlResponseSchema} from './schemas.js';
+import {DocumentNode, graphql, parse, validate} from 'graphql';
 import {schema} from "./schema/schema.js";
-// import {getResolvers} from "./data/resolvers.js";
+import depthLimit from 'graphql-depth-limit'
 
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const { prisma } = fastify;
-  // const resolvers = getResolvers(prisma)
-  // const schema = buildSchema(typeDefs);
+    const {prisma} = fastify;
 
-  fastify.route({
-    url: '/',
-    method: 'POST',
-    schema: {
-      ...createGqlResponseSchema,
-      response: {
-        200: gqlResponseSchema,
-      },
-    },
-    async handler(req) {
-      const { query, variables } = req.body;
+    fastify.route({
+        url: '/',
+        method: 'POST',
+        schema: {
+            ...createGqlResponseSchema,
+            response: {
+                200: gqlResponseSchema,
+            },
+        },
+        async handler(req) {
+            const {query, variables} = req.body;
 
-      const result = await graphql({
-        schema,
-        source: query,
-        variableValues: variables,
-        contextValue: {prisma}
-      });
-      return {
-        data: result.data,
-        errors: result.errors?.map((error) => ({
-          message: error.message,
-        })),
-      };
-    },
-  });
+            const maxDepth = 5;
+            const document = parse(String(query))
+            const validationErrors = validate(schema, document, [depthLimit(maxDepth)]);
+            if (validationErrors.length > 0) {
+                return { errors: validationErrors };
+            }
+
+            const result = await graphql({
+                schema,
+                source: query,
+                variableValues: variables,
+                contextValue: {prisma},
+            });
+            return {
+                data: result.data,
+                errors: result.errors?.map((error) => ({
+                    message: error.message,
+                })),
+            };
+        },
+    });
 };
 
 export default plugin;
